08882a5b3da77a59e14f141b3da6f460e92717e6
Adam Leach <adam.leach@dur.ac.uk>
2021-10-03 16:54:18 +0100

    Fix aircraft testing output

--- aircraft_test.py
+++ aircraft_test.py
@@ -8,7 +8,7 @@ from tqdm import tqdm, trange
 from rotations import *
 from diffusion import ProjectedSO3Diffusion
 
-SAMPLES = 3
+SAMPLES = 8
 
 if __name__ == "__main__":
     import argparse
@@ -51,8 +51,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     device = torch.device(f"cuda") if torch.cuda.is_available() else torch.device("cpu")
-    ds = ShapeNet('train', (0,))
-    dl = DataLoader(ds, batch_size=args.batch, shuffle=True, num_workers=4, pin_memory=True)
+    ds = ShapeNet('test', (0,))
+    dl = DataLoader(ds, batch_size=args.batch, shuffle=False, num_workers=4, pin_memory=True)
+    res = torch.zeros((len(ds), SAMPLES))
 
     net, = init_from_dict(vars(args), RotPredict)
     net.to(device)
@@ -60,22 +61,20 @@ if __name__ == "__main__":
     net.eval()
     process = ProjectedSO3Diffusion(net).to(device)
 
-    ds = ShapeNet('test', (0,))
-    dl = DataLoader(ds, batch_size=args.batch, shuffle=False, num_workers=4, pin_memory=True)
-    res = torch.zeros((len(ds), SAMPLES))
+
     for b, data in enumerate(tqdm(dl, desc = 'batch')):
         proj = PointCloudProj(data.to(device)).to(device)
         process.projection = proj
         results = torch.zeros((args.batch, SAMPLES, 3,3)).to(device)
-        for samp in range(SAMPLES):
+        for samp in trange(SAMPLES, leave=False, desc="sample number"):
             with torch.no_grad():
                 # Initial Haar-Uniform random rotations from QR decomp of normal IID matrix
-                R, _ = torch.qr(torch.randn((args.batch, 3, 3)))
+                R, _ = torch.linalg.qr(torch.randn((args.batch, 3, 3)), "reduced")
                 R = R.to(device)
-                res = torch.zeros((process.num_timesteps, args.batch, 3, 3), device=device)
                 for i in tqdm(reversed(range(0, process.num_timesteps)),
                               desc='sampling loop time step',
                               total=process.num_timesteps,
+                              leave=False,
                               ):
                     R = process.p_sample(R, torch.full((args.batch,), i, device=device, dtype=torch.long)).detach()
             results[:,samp] = R
@@ -83,6 +82,6 @@ if __name__ == "__main__":
         axis, angle = rmat_to_aa(results)
         start = b * args.batch
         end = start + len(angle)
-        res[start:end] = angle.detach().cpu()
+        res[start:end] = angle.detach().cpu().squeeze()
     torch.save(res, "angles.pt")
 
