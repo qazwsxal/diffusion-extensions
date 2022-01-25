3258dcbb931c1820658b316bf4cdd7d52e199102
Adam Leach <adam.leach@dur.ac.uk>
2022-01-25 00:43:49 +0000

    aircraft graph plotting

--- aircraft_diags.py
+++ aircraft_diags.py
@@ -1,5 +1,6 @@
 import torch
 import matplotlib.pyplot as plt
+from util import rmat_to_aa
 
 eul = torch.load("weights/results_aircraft_eul.pt", map_location=torch.device("cpu"))
 eul_flat = eul.ravel()
@@ -9,8 +10,9 @@ so3 = torch.load("weights/results_aircraft_so3.pt", map_location=torch.device("c
 so3_flat = so3.ravel()
 so3_flat_sort, _ = torch.sort(so3_flat)
 
-plt.plot(eul_flat_sort.numpy())
-plt.plot(so3_flat_sort.numpy())
+plt.plot(eul_flat_sort.numpy(), label="euler")
+plt.plot(so3_flat_sort.numpy(), label="so3")
+plt.legend()
 plt.show()
 eul_mean = eul.mean(dim=1)
 eul_std = eul.std(dim=1)
