51d9f5c86f477be82cd766012133f06384e03b62
Adam Leach <adam.leach@dur.ac.uk>
2022-01-26 00:12:03 +0000

    IGSO3 fixes, sample shape and small eps behaviour

--- distributions.py
+++ distributions.py
@@ -14,7 +14,7 @@ class IsotropicGaussianSO3(Distribution):
         self.eps = eps
         self._mean = mean.to(self.eps)
         self._mean_inv = self._mean.transpose(-1, -2)  # orthonormal so inverse = Transpose
-        pdf_sample_locs = pi * torch.linspace(0, 1.0, 200) ** 3.0  # Pack more samples near 0
+        pdf_sample_locs = pi * torch.linspace(0, 1.0, 10000) ** 3.0  # Pack more samples near 0
         pdf_sample_locs = pdf_sample_locs.to(self.eps)
         # As we're sampling using axis-angle form
         # and need to account for the change in density
@@ -23,6 +23,14 @@ class IsotropicGaussianSO3(Distribution):
             pdf_sample_vals = self._eps_ft(pdf_sample_locs) * ((1 - pdf_sample_locs.cos()) / pi)
         # Set to 0.0, otherwise there's a divide by 0 here
         pdf_sample_vals[(pdf_sample_locs == 0).expand_as(pdf_sample_vals)] = 0.0
+        # There's a weird oscillation introduced with small eps, but only for large theta.
+        # manually fix by setting every value from the first one evaluated as negative onwards to 0.0
+        is_neg = pdf_sample_vals < 0.0
+        any_neg = is_neg.any(dim=-1)
+        first_neg = torch.argmax(is_neg.long(), dim=-1)
+        for i, (fn, an) in enumerate(zip(first_neg,any_neg)):
+            if an.item():
+                pdf_sample_vals[i,fn:] = 0.0
         # Trapezoidal intergration
         pdf_val_sums = pdf_sample_vals[..., :-1] + pdf_sample_vals[..., 1:]
         pdf_loc_diffs = torch.diff(pdf_sample_locs, dim=0)
@@ -34,10 +42,10 @@ class IsotropicGaussianSO3(Distribution):
 
     def sample(self, sample_shape=torch.Size()):
         # Consider axis-angle form.
-        axes = torch.randn((*sample_shape, *self.eps.shape, 3)).to(self.eps)
+        axes = torch.randn((*sample_shape, *self.mean.shape[:-2], 3)).to(self.eps)
         axes = axes / axes.norm(dim=-1, keepdim=True)
         # Inverse transform sampling based on numerical approximation of CDF
-        unif = torch.rand((*sample_shape, *self.eps.shape), device=self.trap.device)
+        unif = torch.rand((*sample_shape, *self.mean.shape[:-2]), device=self.trap.device)
         idx_1 = (self.trap < unif[..., None]).sum(dim=-1)
         idx_0 = idx_1 - 1
 
@@ -61,12 +69,21 @@ class IsotropicGaussianSO3(Distribution):
     def _eps_ft(self, t: torch.Tensor) -> torch.Tensor:
         maxdims = max(len(self.eps.shape), len(t.shape))
         # This is an infinite sum, approximate with 5/eps**2 values
-        l_count = round(min((5 / self.eps.min() ** 2).item(), 1e5))
-        if l_count >= 1e6:
-            print("Very small eps!", self.eps.min())
-        l = torch.arange(l_count).reshape((*([1] * maxdims), -1)).to(self.eps)
-        inner = self._eps_ft_inner(l, t[..., None])
-        vals = inner.sum(dim=-1)
+        l_count = round(min((5 / self.eps.min() ** 2).item(), 1e7))
+        if l_count >= 1e4:
+            chunk_size = int(1e4) # Should tune this
+            print("small eps! Using chunked l calcs", self.eps.min())
+            vals = torch.zeros_like(t[None])
+            for l_min in range(0, l_count, chunk_size):
+                l_max = l_min + chunk_size
+                l = torch.arange(l_min, l_max).reshape((*([1] * maxdims), -1)).to(self.eps)
+                inner = self._eps_ft_inner(l, t[..., None])
+                inner_vals = inner.sum(dim=-1)
+                vals += inner_vals
+        else:
+            l = torch.arange(l_count).reshape((*([1] * maxdims), -1)).to(self.eps)
+            inner = self._eps_ft_inner(l, t[..., None])
+            vals = inner.sum(dim=-1)
         return vals
 
     def log_prob(self, rotations):
