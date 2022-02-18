3ca1e2e836b179c1537b0a52847506058b37ccc9
Adam Leach <adam.leach@dur.ac.uk>
2022-02-18 16:30:49 +0000

    Use closed form approximation for IGSO3 PDF

--- distributions.py
+++ distributions.py
@@ -1,6 +1,5 @@
 from math import pi
 
-import torch
 from torch.distributions import Distribution, constraints, Normal, MultivariateNormal
 
 from util import *
@@ -22,19 +21,11 @@ class IsotropicGaussianSO3(Distribution):
             pdf_sample_vals = self._eps_ft(pdf_sample_locs) * ((1 - pdf_sample_locs.cos()) / pi)
         # Set to 0.0, otherwise there's a divide by 0 here
         pdf_sample_vals[(pdf_sample_locs == 0).expand_as(pdf_sample_vals)] = 0.0
-        # There's a weird oscillation introduced with small eps, but only for large theta.
-        # manually fix by setting every value from the first one evaluated as negative onwards to 0.0
-        is_neg = pdf_sample_vals < 0.0
-        any_neg = is_neg.any(dim=-1)
-        first_neg = torch.argmax(is_neg.long(), dim=-1)
-        for i, (fn, an) in enumerate(zip(first_neg, any_neg)):
-            if an.item():
-                pdf_sample_vals[i, fn:] = 0.0
+
         # Trapezoidal intergration
         pdf_val_sums = pdf_sample_vals[..., :-1] + pdf_sample_vals[..., 1:]
         pdf_loc_diffs = torch.diff(pdf_sample_locs, dim=0)
         self.trap = (pdf_loc_diffs * pdf_val_sums / 2).cumsum(dim=-1)
-        self.trap /= self.trap[:, -1, None].clone()
         self.trap_loc = pdf_sample_locs[1:]
         super().__init__()
 
@@ -65,23 +56,11 @@ class IsotropicGaussianSO3(Distribution):
         return out
 
     def _eps_ft(self, t: torch.Tensor) -> torch.Tensor:
-        maxdims = max(len(self.eps.shape), len(t.shape))
-        # This is an infinite sum, approximate with 5/eps**2 values
-        l_count = round(min(max(1e3, (5 / self.eps.min() ** 2).item()), 1e7))
-        if l_count >= 1e5:
-            chunk_size = int(1e5)  # Should tune this
-            print("small eps! Using chunked l calcs", self.eps.min())
-            vals = torch.zeros_like(t[None])
-            for l_min in range(0, l_count, chunk_size):
-                l_max = l_min + chunk_size
-                l = torch.arange(l_min, l_max).reshape((*([1] * maxdims), -1)).to(self.eps)
-                inner = self._eps_ft_inner(l, t[..., None])
-                inner_vals = inner.sum(dim=-1)
-                vals += inner_vals
-        else:
-            l = torch.arange(l_count).reshape((*([1] * maxdims), -1)).to(self.eps)
-            inner = self._eps_ft_inner(l, t[..., None])
-            vals = inner.sum(dim=-1)
+        vals = sqrt(pi) * self.eps ** (-3 / 2) * torch.exp(self.eps / 4) * torch.exp(-((t / 2) ** 2) / self.eps) \
+               * (t - torch.exp((-pi ** 2) / self.eps)
+                  * ((t - 2 * pi) * torch.exp(pi * t / self.eps) + (t + 2 * pi) * torch.exp(-pi * t / self.eps))
+                  ) / (2 * torch.sin(t / 2))
+        vals[vals.isinf()] = 0.0
         return vals
 
     def log_prob(self, rotations):
