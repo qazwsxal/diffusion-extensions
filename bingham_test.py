35a8d548b6ff05a7fb6d8a9793c6b6c9dfbdced1
Adam Leach <adam.leach@dur.ac.uk>
2022-02-21 10:37:18 +0000

    Low GPU usage, so use a mp pool and run 4 sampling procs at once.

--- bingham_test.py
+++ bingham_test.py
@@ -7,11 +7,31 @@ import pickle
 SAMPLES = 100_000
 NET_SAMPLES = 100_000
 NET_RUNS = SAMPLES//NET_SAMPLES
+device = torch.device(f"cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+
+def calc_step(acro, cov, step):
+    net = RotPredict(out_type="skewvec").to(device)
+    net.load_state_dict(torch.load(f"weights/weights_bing_{acro}_{step}.pt", map_location=device))
+    diff = SO3Diffusion(net, loss_type="skewvec").to(device)
+
+    bing = Bingham(loc=loc.to(device), covariance_matrix=cov.to(device))
+    bing_samples = bing.sample((SAMPLES,))
+    bing_samples = quat_to_rmat(bing_samples)
+    diff_start = IsotropicGaussianSO3(eps=torch.ones(1, device=device))
+    diff_samples = []
+    for i in range(NET_RUNS):
+        R = diff.p_sample_loop((NET_SAMPLES,))
+        diff_samples.append(R)
+    diff_samples = torch.cat(diff_samples, dim=0)
+    print('aaaa')
+    mmd = MMD(bing_samples, diff_samples, rmat_gaussian_kernel)
+    results[step] = mmd.item()
 
 if __name__ == "__main__":
-    import tqdm
     import argparse
-
+    import torch.multiprocessing as mp
+    mp.set_start_method('spawn') # need so cuda can be used in multiple threads
     parser = argparse.ArgumentParser(description="Aircraft rotation args")
     parser.add_argument(
         "cov", type=str, help="covariance matrix to use", choices = ["sur", "scr", "lur", "lcr"]
@@ -19,23 +39,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     acro = args.cov
     cov, = [c for _, a, c in covpairs if a == acro]
-    device = torch.device(f"cuda") if torch.cuda.is_available() else torch.device("cpu")
     results = {}
-    for step in tqdm.trange(1000, 50001, 5000):
-        net = RotPredict(out_type="skewvec").to(device)
-        net.load_state_dict(torch.load(f"weights/weights_bing_{acro}_{step}.pt", map_location=device))
-        diff = SO3Diffusion(net, loss_type="skewvec").to(device)
-
-        bing = Bingham(loc=loc.to(device), covariance_matrix=cov.to(device))
-        bing_samples =  bing.sample((SAMPLES,))
-        bing_samples =quat_to_rmat(bing_samples)
-        diff_start = IsotropicGaussianSO3(eps=torch.ones(1, device=device))
-        diff_samples = []
-        for i in tqdm.trange(NET_RUNS):
-            R = diff.p_sample_loop((NET_SAMPLES,))
-            diff_samples.append(R)
-        diff_samples = torch.cat(diff_samples, dim=0)
-        print('aaaa')
-        mmd = MMD(bing_samples, diff_samples, rmat_gaussian_kernel)
-        results[step] = mmd.item()
+    eval_points = [(acro, cov, step) for step in range(1000, 50001, 5000)]
+    with mp.Pool(processes=4) as pool:
+        p_results = pool.starmap(calc_step, eval_points)
+    for (acro, step), mmd in zip(eval_points, p_results):
+        results[step] = mmd
     pickle.dump(results, open(f'bingham_mmd_{acro}.pkl', 'wb'))
